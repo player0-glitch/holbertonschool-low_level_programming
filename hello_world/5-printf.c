619fa8bb803b5ccb532f19fefad1e5f51818db40
player0-glitch <mokhaditlhalefo@gmailcom>
2026-01-30 11:30:40 +0000

    c code wit betty

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art,");
+	/* return code */
+	return (0);
+}
