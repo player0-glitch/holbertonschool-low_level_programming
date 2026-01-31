ab429a4f5046346c98eaa66d5048f63d366ae5b1
player0-glitch <mokhaditlhalefo@gmail.com>
2026-01-31 14:41:41 +0000

    C code

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+	putchar('\n');
+	return (0);
+}
+
