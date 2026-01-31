99a797f23df0bf023c08216757f8ddef20873231
player0-glitch <mokhaditlhalefo@gmail.com>
2026-01-31 14:15:29 +0000

    C code

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - 0 on success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
