64ed8c318df569c19ed01cd116b35cfef9435ef7
player0-glitch <mokhaditlhalefo@gmail.com>
2026-01-31 14:46:01 +0000

    C code

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--) 
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
