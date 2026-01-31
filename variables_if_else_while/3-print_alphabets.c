680f7bef899af931471333a624a3b7315621d23c
player0-glitch <mokhaditlhalefo@gmail.com>
2026-01-31 14:27:16 +0000

    C code

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 65; n < 91; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
+
