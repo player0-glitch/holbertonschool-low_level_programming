7468dbabdd2782e51f1ac276dd1d620418b1670b
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-02 06:50:33 +0000

    C code 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,9 +7,8 @@ void print_alphabet(void)
 	int n;
 
 	for (n = 97; n < 123; n++)
-	{
 		_putchar(n);
-	}
+
 	_putchar('\n');
 }
 
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the charater c to stdout
