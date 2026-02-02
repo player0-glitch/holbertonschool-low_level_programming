74fb5b5b155bb80424b2f693252f89d93e7b7afc
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-02 06:46:45 +0000

    C code 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints out alphabet using wrapper for write syscall
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
