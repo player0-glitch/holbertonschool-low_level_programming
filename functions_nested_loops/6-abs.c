47e340daafbbeeae864447809d0409f67299ab0d
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-02 09:32:20 +0000

    C code abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - finds the absolute value of a number
+ *
+ * @n: number whose absolute value will be calculated
+ * Return: returns the absolute value of n
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
