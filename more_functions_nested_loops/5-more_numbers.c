4f352d41e7ea7dc2e8bdb55cecb9bcd1306bd3d2
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-03 07:25:36 +0000

    more C code

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,21 @@ void more_numbers(void)
 {
 	int it, n;
 
+	char c;
+
 	for (it = 0; it < 10; it++)
 	{
-		for (n = 0; n < 14; n++)
-			_putchar('0' + n);
+		for (n = 0; n < 15; n++)
+		{
+			c = n;
+			if (n >= 10)
+			{
+				c = (n / 10);
+				_putchar('0' + c);
+				c = (n % 10);
+			}
+			_putchar('0' + c);
+		}
 		_putchar('\n');
 	}
 }
