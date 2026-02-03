55bf1b5e715a178b12ea824a88d561daa6de54bb
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-03 05:54:28 +0000

    More C Code

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar('0' + n);
+	}
+	_putchar('\n');
+}
+
