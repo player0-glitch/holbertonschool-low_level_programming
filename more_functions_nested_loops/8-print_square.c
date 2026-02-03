34fd7160eaf46c8eed5e6688b39047b74f87edfd
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-03 09:50:11 +0000

    more C code

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_square - print_square
+  * @size: squared
+  */
+void print_square(int size)
+{
+	int i, j;
+
+	i = size;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
