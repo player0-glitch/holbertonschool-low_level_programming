649140b6357c199c2ec38e328f9c16c8ccc102d6
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-04 13:52:23 +0000

    C code concat string

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate head src to dest tail
+ *
+ * @src: source string
+ *
+ * @dest: destination string
+ *
+ * Return: pointer to concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *d = dest; /*pointer to destination*/
+	char *s = src; /* pointer ro sourc string*/
+
+	while (*d != '\0')
+		d++;
+	/* d should now be a null terminator */
+	while (*s != '\0')
+	{
+		*d = *s;
+		d++;
+		s++;
+	}
+	*d = '\0';
+	return d;
+}
