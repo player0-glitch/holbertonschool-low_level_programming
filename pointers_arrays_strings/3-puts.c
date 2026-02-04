a0e6be778f4a81396f8361f3658b0d7d6b281ebb
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-04 07:33:55 +0000

    C code puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints out string
+ *
+ * @str: string to printerd to stdout
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-void _puts(char *c);
+void _puts(char* str);
 int _strlen(char *s);
 void print_rev(char *s);
 #endif
