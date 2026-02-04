f0ac46a35f8fa919507cc6651f58561e6891c011
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-04 10:00:20 +0000

    C code reverse string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,26 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
  * rev_string -  reverse a string in-place
+ *
+ * @s: string head pointer to reverse
  */
-void rev_string(char *s) {}
+void rev_string(char *s)
+{
+	char c = 0;
+	char *i = s;
+	char *j = s;
+
+	while ((*j) != '\0')
+		j++;
+
+	j--;
+	while (i < j)
+	{
+		c = *i;
+		*i = *j;
+		*j = c;
+		i++;
+		j--;
+	}
+}
