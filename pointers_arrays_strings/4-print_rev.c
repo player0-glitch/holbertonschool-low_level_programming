db173213d2de13ca5e75a54ebebd3b3fddf32361
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-04 09:10:24 +0000

    C code reverse string print

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * print_rev - write string in reverse using pointers
+ *
+ * @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int i = 0, len;
+	char c;
+
+	while (s[i] != '\0')
+		i++;
+	len = i - 1;
+	for (i = len; i >= 0; --i)
+		write(1, &s[i], 1);
+	c = '\n';
+	write(1, &c, 1);
+}
