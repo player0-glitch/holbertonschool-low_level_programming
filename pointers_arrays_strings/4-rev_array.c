68fbb8a50186c483f5e81ac2b6aceff1614757be
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-07 21:47:51 +0000

    C Code

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,14 +5,16 @@
  * @a: an array of integers
  * @n: the number of elements to reverse
  */
-void reverse_array(int *a, int n) {
-    int i = 0;
-    int temp;
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
 
-    while (i < n / 2) {
-        temp = a[i];
-        a[i] = a[n - 1 - i];
-        a[n - 1 - i] = temp;
-        i++;
-    }
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+		i++;
+	}
 }
