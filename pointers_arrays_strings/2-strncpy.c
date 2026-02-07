538131423a33274c6995cf24f5c9d8cb0f567cfa
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-07 21:37:02 +0000

    C Code

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,20 +5,26 @@
  *
  * @dest: destination string
  *
- * @char *src: source string
+ * @src: source string
  *
  * @n: number of bytes to copy
+ *
+ * Return: dest pointer
  */
-char *_strncpy(char *dest, char *src, int n) {
-    int i = 0;
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
 
-    while (i < n && src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
-    }
-    while (i < n) {
-        dest[i] = '\0';
-        i++;
-    }
-    return dest;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
