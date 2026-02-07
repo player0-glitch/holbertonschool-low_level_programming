32483a39a779a13142c0b7c278b12fe7c5d893c1
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-07 21:45:45 +0000

    C Code

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,18 @@
  *
  * @s2: second string
  *
- * Return: 0 if s1 and s2 are equal, a negative number if s1 < s2, and a positive number if s1 > s2
+ * Return: 0 if s1 and s2 are equal, a negative number if s1 < s2
  */
-int _strcmp(char *s1, char *s2) {
-    int i = 0;
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] != s2[i])
-            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        i++;
-    }
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
 
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
