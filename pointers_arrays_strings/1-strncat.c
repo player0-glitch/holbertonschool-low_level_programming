663ef3db74f7632a92329542bd44a737deef4eb9
player0-glitch <mokhaditlhalefo@gmail.com>
2026-02-07 21:26:37 +0000

    C Code

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,19 @@
  *
  * Return: pointer to concatenated string
  */
-char *_strncat(char *dest, char *src, int n) {
-    int i = 0, j = 0;
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
 
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+		i++;
 
-    while (j < n && src[j] != '\0') {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    dest[i] = '\0';
-    return dest;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
