c8c34f0881b318d2023c9f8fc320615419ec3171
player0-glitch <mokhaditlhalefo@gmail.com>
2026-03-02 13:14:59 +0000

    betty on c code 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - print sum of diagonal square
@@ -10,17 +11,21 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		/* diag 1 */
-		sum += a[i][i];
+		sum1 += a[i * size + i];
 
 		/* diag 2 */
-		sum += a[i][size - i - 1];
+		sum2 += a[i * size + (size - i - 1)];
 	}
 	/* if the array is of size 2n+1 */
 	if (size % 2 != 0)
-		sum -= a[size / 2][size / 2];
+	{
+		sum1 -= a[size / 2 * size + (size / 2)];
+		sum2 -= a[size / 2 * size + (size / 2)];
+	}
 }
