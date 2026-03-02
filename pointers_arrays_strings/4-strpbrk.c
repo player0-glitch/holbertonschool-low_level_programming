b57755a5f3830e9dc3f4d7d01d8cacc50765d4ca
player0-glitch <mokhaditlhalefo@gmail.com>
2026-03-02 11:20:29 +0000

    betty checks

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -15,9 +15,9 @@ char *_strpbrk(char *s, char *accept)
 	if (s == NULL || accept == NULL)
 		return (NULL);
 
-	for (p1 = s; p1 != '\0'; p1++)
+	for (p1 = s; *p1 != '\0'; p1++)
 	{
-		for (p2 = accept; p2 != '\0'; p2++)
+		for (p2 = accept; *p2 != '\0'; p2++)
 		{
 			if (*p1 == *p2)
 				return (p1);
