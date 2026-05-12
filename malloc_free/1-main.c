cd35638dc520079635a706a91caa239615acf850
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-12 08:55:53 +0200

    format fixes

--- malloc_free/1-main.c
+++ malloc_free/1-main.c
@@ -7,15 +7,17 @@
  *
  * Return: Always 0.
  */
-int main(void) {
-  char *s;
+int main(void)
+{
+	char *s;
 
-  s = _strdup("Duplicated");
-  if (s == NULL) {
-    printf("failed to allocate memory\n");
-    return (1);
-  }
-  printf("%s\n", s);
-  free(s);
-  return (0);
+	s = _strdup("Duplicated");
+	if (s == NULL)
+		{
+			printf("failed to allocate memory\n");
+			return (1);
+		}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
