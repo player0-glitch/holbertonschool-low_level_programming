7b10a862788813d6e727873275f10194c2115ad9
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-12 09:09:56 +0200

    remove strlen

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,20 +11,20 @@
 char *_strdup(char *str)
 {
 	char *dest = NULL;
-	size_t len;
+	size_t len = 0;
 
 	if (str == NULL)
 		return (NULL);
 	dest = str;
 	while (*dest++)
 		len++;
-	dest = (char *)malloc(len * sizeof(str));
+	dest = (char *)malloc(len * sizeof(char));
 
 	/*incase OS fails to give memory*/
 	if (dest == NULL)
 		return (NULL);
 
 	memcpy(dest, str, len);
-
+	/* dest[(len + 1)] = '\0'; */
 	return (dest);
 }
