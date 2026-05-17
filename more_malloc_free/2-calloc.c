84685b151475f8198c6d5f20e1d0ab3320ee85c2
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-17 05:40:17 +0200

    fixed formatting

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,1 +1,20 @@
 #include "main.h"
+#include <stdlib.h>
+
+/** _calloc- function to allocate  memory using malloc and zero'd out
+ * @nmemb: members to be allocated for
+ * @size: size of allocated memeory
+ *
+ * Returns: void * for allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ret = NULL;
+
+	if (size < 1 || nmemb < 1)
+		return (NULL);
+	ret = calloc(nmemb, size);
+	if (!ret)
+		return (NULL);
+	return (ret);
+}
