d5c7df52e40e22feee956a8d5c183c722f39c320
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-17 10:08:24 +0200

    fix formatting

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog
+typedef struct
 {
 	float age;
 	const char *owner;
 	const char *name;
-};
+} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 #endif
