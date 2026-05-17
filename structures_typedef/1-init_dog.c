33a135d85160f5f52aa5bdab66d3bab00e8e496d
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-17 09:10:16 +0200

    typedef and structs

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - constructor of the dog struct
+ * @d: pointer to struct
+ * @name: to be set for the dog struct
+ * @age: age for dog struct
+ * @owner: name of the dog struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,17 +6,6 @@ struct dog
 	const char *owner;
 	const char *name;
 };
-/**
- * init_dog - constructor of the dog struct
- * @d: pointer to struct
- * @name: to be set for the dog struct
- * @age: age for dog struct
- * @owner: name of the dog struct
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
