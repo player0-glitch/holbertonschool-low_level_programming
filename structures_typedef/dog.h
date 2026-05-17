9a9b336d9934ab13a84e4255d45a0491193e8a42
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-17 23:02:50 +0200

    fix formatting

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,6 +11,7 @@ typedef struct
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 /*Utility*/
 void copy_str(char *d, char *s);
 
