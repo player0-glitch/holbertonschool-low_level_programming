a5598185244a5967cc29c362fcbc1e7c460020c9
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-17 09:53:32 +0200

    fix formatting

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a dog struct
+ * @d: struct to be printed
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	/*only name or owner can be null*/
+	if (!d->name)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	if (!d->owner)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
