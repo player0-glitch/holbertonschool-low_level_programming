33e2a9d6da25da7eba6b0db89a46beddf6942057
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-19 12:09:50 +0200

    fix return

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,18 +14,25 @@ int main(int argc, char *argv[])
 
 	/*First get all the command line arguements*/
 	if (argc != 4)
-		exit(0);
+		{
+			printf("Error\n");
+			exit(98);
+		}
 	a = (int)atoi(argv[1]);
 	b = (int)atoi(argv[3]);
+	if (b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
 
 	my_math.op = argv[2];
-
 	my_math.f = get_op_func(my_math.op);
 
 	if (!my_math.f)
 		{
 			printf("Error\n");
-			exit(0);
+			exit(99);
 		}
 	printf("%d\n", my_math.f(a, b));
 
