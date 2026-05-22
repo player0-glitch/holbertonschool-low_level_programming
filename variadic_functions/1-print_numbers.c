c4a2e4d0126f9e23dae980605d908834ed92225f
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-22 15:37:20 +0200

    fixed var_arg issue

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; ++i)
 		{
-			printf("%d", i);
+			printf("%d", va_arg(args, const unsigned int));
 			if (i == (n - 1))
 				break;
 			printf("%s", separator);
