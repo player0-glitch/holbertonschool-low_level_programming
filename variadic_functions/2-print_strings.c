382d1542f48c13fd6092d228aafbcce177c56c64
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-25 09:30:16 +0200

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints numbers separated delimiter
- *
- * @separator: a separator for our listed numbers
- * @n: number of arguements */
-void print_numbers(const char *separator, const unsigned int n, ...)
+ * print_strings - prints n strings
+ *@seperator: separator that will be used
+ *@n: number of the strings to print
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i;
+	int i;
 	if (n < 1)
 		return;
 	va_start(args, n);
 
 	for (i = 0; i < n; ++i)
 		{
-			printf("%d", va_arg(args, const unsigned int));
+			printf("%s", va_arg(args, const char *));
 			if (i == (n - 1))
 				break;
 			if (!separator)
 				continue;
 			printf("%s", separator);
 		}
-
 	printf("\n");
 }
