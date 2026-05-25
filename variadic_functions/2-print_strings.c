89d245bf0fb0bf20d8a515195d95201a8062f74a
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-25 12:04:03 +0200

    fix formatting

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			const char *c = va_arg(args, const char *);
 
 			if (!c)
-				printf("(nil");
+				printf("(nil)");
 			else
 				printf("%s", c);
 
