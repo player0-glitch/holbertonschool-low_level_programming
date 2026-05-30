d8f46dae1867cd989aa688036ada6eb68a538702
player0-glitch <mokhaditlhalefo@gmail.com>
2026-05-30 15:09:51 +0200

    doubly linked list

---
+++ doubly_linked_list/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <cstddef>
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+size_t dlistint_len(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
+#endif
